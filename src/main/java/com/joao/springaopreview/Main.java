793e6c590268990770944bfa482f8561151c3904
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-03 19:43:11 -0500

    implementing basic aspect

--- src/main/java/com/joao/springaopreview/Main.java
+++ src/main/java/com/joao/springaopreview/Main.java
@@ -1,7 +1,17 @@
 package com.joao.springaopreview;
 
+import com.joao.springaopreview.config.SpringConfig;
+import com.joao.springaopreview.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        accountDAO.addAccount();
+
+        context.close();
     }
 }
