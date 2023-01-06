2b5196b3ba94e99c7244dd73580910594b5422d8
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-05 19:55:10 -0500

    Pointcut declaration

--- src/main/java/com/joao/springaopreview/aspect/LoggingAspect.java
+++ src/main/java/com/joao/springaopreview/aspect/LoggingAspect.java
@@ -2,14 +2,24 @@ package com.joao.springaopreview.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* com.joao.springaopreview.dao.*.*(..))")
+    @Before("execution(* com.joao.springaopreview.dao.*.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @before advice on any method start with add");
+    }
+
+    @Before("myPointCutMethod()")
+    public void beforePointcut() {
+        System.out.println("\n====>>> Executing @before advice for pointcut");
+    }
+
+    @Pointcut("execution(boolean com.joao.springaopreview.dao.MembershipDAO.addMember())")
+    public void myPointCutMethod() {
     }
 }
